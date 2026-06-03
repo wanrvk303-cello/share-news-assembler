469cfa25d356c8fb89f9edf997d2a9636303b4e2
wanrvk <wangav303@gmail.com>
2026-06-03 16:34:33 +0300

    feat: add RSS feed source configurations (Reuters, CNBC, Yahoo, etc.)

---
+++ app/feeds.py
@@ -0,0 +1,37 @@
+FEED_SOURCES = [
+    {
+        "name": "Reuters Business",
+        "url": "https://feeds.reuters.com/reuters/businessNews",
+        "type": "market_wide",
+    },
+    {
+        "name": "CNBC Top News",
+        "url": "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=100003114",
+        "type": "market_wide",
+    },
+    {
+        "name": "MarketWatch Top Stories",
+        "url": "https://feeds.marketwatch.com/marketwatch/topstories/",
+        "type": "market_wide",
+    },
+    {
+        "name": "Yahoo Finance",
+        "url": "https://finance.yahoo.com/news/rssindex",
+        "type": "market_wide",
+    },
+    {
+        "name": "Bloomberg Markets",
+        "url": "https://feeds.bloomberg.com/markets/news.rss",
+        "type": "market_wide",
+    },
+    {
+        "name": "Seeking Alpha Market News",
+        "url": "https://seekingalpha.com/feed.xml",
+        "type": "stock_specific",
+    },
+    {
+        "name": "Investing.com News",
+        "url": "https://www.investing.com/rss/news.rss",
+        "type": "market_wide",
+    },
+]
